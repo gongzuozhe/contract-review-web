b3f459ffe39a022fdf028de898a527fc5914c429
gongzuozhe <you@example.com>
2026-03-30 22:52:01 +0800

    Update config for Netlify

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  outputExport: 'next',
   experimental: {
     serverActions: {
       bodyParser: false
     }
+  },
+  images: {
+    unoptimized: true
   }
 }
 
